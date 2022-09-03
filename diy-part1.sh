8c8cae3902f4018f5f0e80fef41cda0b9f30b9ae
Aierk <57520436+Aierklk@users.noreply.github.com>
2022-09-03 13:08:32 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,15 +12,8 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git' feeds.conf.default
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.defa
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-
-# Add cloudreve
-git clone https://github.com/kiddin9/openwrt-packages
-mv openwrt-packages/cloudreve package
-mv openwrt-packages/luci-app-cloudreve package
-echo 'Installed cloudreve'
